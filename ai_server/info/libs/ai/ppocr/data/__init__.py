fc940072854a9a181344e6dffbcedbe69fdd7f11
yuemengrui <1401190335@qq.com>
2023-12-04 14:05:38 +0800

    update

--- ai_server/info/libs/ai/ppocr/data/__init__.py
+++ ai_server/info/libs/ai/ppocr/data/__init__.py
@@ -9,7 +9,7 @@ import sys
 __dir__ = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.abspath(os.path.join(__dir__, '../..')))
 
-from info.libs.ai.ppocr.data.imaug import transform, create_operators
+from .imaug import transform, create_operators
 
 __all__ = [
     'transform', 'create_operators'
