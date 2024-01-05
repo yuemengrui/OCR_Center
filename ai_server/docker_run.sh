bc4647669a529e8650b17be0de21bf45b2dbb85a
yuemengrui <1401190335@qq.com>
2024-01-05 09:54:39 +0800

    update

--- ai_server/docker_run.sh
+++ ai_server/docker_run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 cd /root/workspace/OCR_Center/ai_server && CUDA_VISIBLE_DEVICES=0 nohup python manage_ocr_center.py >/dev/null 2>&1 &
-
-while true; do
-  sleep 6
-done
+echo "server runing"
+/bin/bash
